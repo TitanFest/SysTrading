0d0c4dbd20da936c89de80f5dfe7a324aa75807d
Alberto <albertofester@unicesar.edu.co>
2024-06-08 12:52:09 -0500

    "Se añadio Products a models"

--- services/products.js
+++ services/products.js
@@ -7,18 +7,57 @@ const initModels = require("../models/init-models");
 const products = require("../models/products");
 const models = initModels(sequelize);
 
-async function registrar(product) {
+async function registrar(Product) {
   try {
-    const dbProduct = await models.products.create({ ...product });
+    const dbProduct = await models.products.create({ ...Product });
     return {
       mensaje: "Producto creado exitosamente",
-      idProduct: dbProduct.idproduct,
     };
   } catch (error) {
     console.log(error);
-    return { message: "No se pudo crear el producto" };
+    return { message: "No se pudo crear el producto"+error };
   }
 }
+
+async function ListaProductos() {
+  try { 
+    const productos = await models.products.findAll();
+    return productos;
+  } catch (error) {
+    console.log(error);
+    return { mensaje: "No se pudo obtener la lista de productos" };
+  }
+}
+async function actualizarProducto(id, product) {
+  try {
+
+    await models.products.update(product, {
+      where: { idproduct: id }
+    });
+    
+    return { mensaje: "Producto actualizado exitosamente" };
+  } catch (error) {
+    console.log(error);
+    return { mensaje: "No se pudo actualizar el producto" };
+  }
+}
+
+
+async function eliminarProducto(id) {
+  try {
+    await models.products.destroy({
+      where: { idproduct: id }
+    });
+    return { mensaje: "Producto eliminado exitosamente" };
+  } catch (error) {
+    console.log(error);
+    return { mensaje: "No se pudo eliminar el producto " };
+  }
+}
+
 module.exports = {
   registrar,
+  ListaProductos,
+  actualizarProducto,
+  eliminarProducto
 };
